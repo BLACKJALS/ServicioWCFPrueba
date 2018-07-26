f40362cce853213bc75fafe18dce231db43f0969
alejandro.leon <callinferno_666@hotmail.com>
2018-07-26 15:59:57 -0500

    Servicio WCF V1.0

---
+++ ServicioTutorial/Servicios/IServicioPrueba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioTutorial.Servicios {
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioPrueba" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioPrueba {
+        [OperationContract]
+        void cargaPruebaExcel(FileStream file);
+
+        [OperationContract]
+        string enviarCorreo(string correo);
+    }
+}
---
+++ ServicioTutorial/Servicios/ServicioPrueba.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioTutorial.Servicios {
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioPrueba" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServicioPrueba.svc o ServicioPrueba.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServicioPrueba : IServicioPrueba {
+        public void cargaPruebaExcel(FileStream file) {
+            var prueba = file;
+        }
+
+        public string enviarCorreo(string correo) {
+            return correo;
+        }
+    }
+}
